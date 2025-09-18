9401f607f0b5e4ceb451b39fc5b6c554d155dde7
Joshua Ajowele <joshuaajowele@gmail.com>
2025-09-18 15:59:33 -0400

    exercise complete

--- code/gis01.R
+++ code/gis01.R
@@ -6,9 +6,10 @@ if (!require(pacman)) install.packages("pacman")
 
 pacman::p_load(tidyverse,
                sf,
-               mapview)
+               mapview,
+               here)
 #read in fish data
-df_fish <- read_csv("data/data_finsync_nc.csv")
+df_fish <- read_csv(here::here("data/data_finsync_nc.csv"))
 
 #convert dataframe/tibblee to georeferenced data
 sf_site <- df_fish %>% 
@@ -24,7 +25,7 @@ mapview(sf_site,
 
 #save new data as a r file
 saveRDS(sf_site,
-        file = "data/sf_finsync_nc.rds")
+        file =here::here("data/sf_finsync_nc.rds"))
 
 #convert from geodetic to projected####
 #subset to two rows for 
@@ -41,3 +42,43 @@ print(sf_ft_utm)
 
 #distance between two points
 st_distance(sf_ft_utm)
+
+
+#Exercise####
+#load data and convert to tibble
+df_quakes<-as_tibble(quakes)
+
+sf_quakes <- df_quakes %>% 
+  distinct(stations, long, lat) %>% # get unique combinations of longitude & latitude
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4326)
+
+print(sf_quake)
+
+#view map
+mapview(sf_quakes,
+        legend = FALSE)
+#convert from geodetic to projected####
+#subset to two rows for 
+sf_ft_quake <- sf_quakes %>% 
+  slice(c(1, 2))
+
+print(sf_ft_quake)
+#change geodetic to projected (3 dimen to 2 dimension, using UTM60S)
+sf_ft_utm_quake <- sf_ft_quake %>% 
+  st_transform(crs = 32760)
+
+print(sf_ft_utm_quake)
+
+
+#distance between two points
+st_distance(sf_ft_utm_quake)
+mapview(sf_ft_utm_quake)
+
+#calculating distance before projection based on approximation
+st_distance(sf_ft_quake)#close to the previous
+
+
+#save as rds file
+saveRDS(sf_quakes, file = here::here("data/sf_quakes.rds"))
+
